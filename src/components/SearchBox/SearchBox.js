66a27dd880307bd9e0d271c72b50a8ba6d6a4adc
Matthew Beatty <beattyml1@gmail.com>
2018-06-09 15:22:24 -0400

    Debounce the search button/try out some RxJS

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,25 +1,32 @@
 // props: { onSearch: function, label, onValueChange }
 import * as React from "react";
 import './SearchBox.css';
+import { debounceTime } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import {preventDefault} from '../../services/PreventDefault'
 
 export class SearchBox extends React.Component {
     constructor() {
         super();
         this.state = { query: '' }
+        this.search$ = new Subject();
+        this.search$.pipe(debounceTime(600)).subscribe(() => this.props.onSearch(this.state.query));
     }
+
+
     onValueChange = e => {
         this.setState({query: e.target.value});
         if (this.props.onValueChange) this.props.onValueChange(e.target.value);
     };
     onSearch = e => {
-        this.props.onSearch(this.state.query);
+        this.search$.next();
         e.preventDefault();
         return false;
     }
 
     render = () => {
         return (
-            <form role={"search"} onSubmit={this.onSearch} className={"search"}>
+            <form role={"search"} onSubmit={preventDefault(() => this.search$.next())} className={"search"}>
                 <input type={"search"} role={"search"} name={"query"} placeholder={this.props.label} value={this.state.query||''}
                        aria-label={this.props.label} className={"search-box"} onChange={this.onValueChange}
                        autoFocus={true} spellCheck="false"/>
